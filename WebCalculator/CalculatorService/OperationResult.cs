fb245aadedcc865ad8e0ddb4ee564376b60ed21d
Carlos Acosta <carlos9822@hotmail.com>
2019-09-15 16:35:17 -0400

    commit

---
+++ WebCalculator/CalculatorService/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public class OperationResult
+    {
+        public double FirstNumber { get; set; } = 0;
+        public double DoubleNumber { get; set; } = 0;
+        public double Result { get; set; } = 0;
+    }
+}
